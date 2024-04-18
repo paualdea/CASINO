18a355a2874702c42c54687d14f91538eb7b9bb4
Pau Aldea <paualdeabatista@protonmail.com>
2024-04-18 11:38:07 +0200

    configuracion JavaFX

---
+++ code/src/gui/Main.java
@@ -0,0 +1,21 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        primaryStage.setTitle("CASINO");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
