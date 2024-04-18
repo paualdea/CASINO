9c3ccd11413b222819248378787303bbb2456e8d
Pau Aldea <paualdeabatista@protonmail.com>
2024-04-18 12:01:50 +0200

    nuevas clases

--- code/src/gui/Main.java
+++ code/src/gui/Main.java
@@ -1,9 +1,11 @@
 package gui;
 
+import java.io.File;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,6 +16,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        File file = new File("img/favicon.png");
+        
+        Image favicon = new Image(file.toURI().toString());
+        primaryStage.getIcons().add(favicon);
+        
         primaryStage.setTitle("CASINO");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
