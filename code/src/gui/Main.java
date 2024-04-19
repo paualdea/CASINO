d14239c1035313129ea53879ebe545e6e531126c
Pau Aldea <paualdeabatista@protonmail.com>
2024-04-19 12:08:44 +0200

    cambio de pantalla

--- code/src/gui/Main.java
+++ code/src/gui/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("PaginaInicio.fxml"));
         
         primaryStage.getIcons().add(new Image("/img/favicon.png"));
         
