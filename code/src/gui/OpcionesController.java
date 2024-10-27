09d9b0a597ec42f8d0d1b9e4b6e3eb9a60ed81ad
Pau Aldea Batista <paualdeabatista@protonmail.com>
2024-10-27 12:34:57 +0100

    v6.0.0 - sistema reset datos opciones

--- code/src/gui/OpcionesController.java
+++ code/src/gui/OpcionesController.java
@@ -3,6 +3,9 @@ package gui;
 import casino.CASINO;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,9 +15,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.text.Text;
 import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
-
 
 public class OpcionesController implements Initializable {
     
@@ -23,6 +26,10 @@ public class OpcionesController implements Initializable {
     private Button atras;
     @FXML
     private CheckBox fcheck;
+    @FXML
+    private Button reset;
+    @FXML
+    private Text warning;
     
     // Importamos clase CASINO
     private CASINO casino = Main.getCasino();
@@ -31,6 +38,7 @@ public class OpcionesController implements Initializable {
     private final double baseWidth = 1066;
     private final double baseHeight = 600;
     
+    private int borrarBoton = 0;
     
     /**
      * Clase que inicializa
@@ -43,6 +51,13 @@ public class OpcionesController implements Initializable {
         
         // Cambiamos el valor del checkbox en funcion de la variable fullscreen
         fcheck.setSelected(fullscreen);
+        
+        // Establecemos la variable borrarBoton a 0
+        borrarBoton = 0;
+        // Establecemos el texto del warning
+        warning.setText("Click another time to reset database");
+        // Ocultamos warning borrado datos
+        warning.setVisible(false);
     }
     
     /**
@@ -113,5 +128,74 @@ public class OpcionesController implements Initializable {
             // Restablecemos la escala cuando salimos de fullscreen
             root.getTransforms().clear();
         }
+    }
+    
+    /**
+     * Funcion para resetear la base de datos
+     * 
+     * @param event 
+     */
+    @FXML
+    void resetData(ActionEvent event) throws SQLException, IOException, InterruptedException {
+        // Cuando hacemos clic sobre el boton se suma uno a borrarBoton
+        borrarBoton += 1;
+        
+        if (borrarBoton == 1) {
+            warning.setVisible(true);
+        } 
+        
+        else if (borrarBoton == 2) {
+            // Ejecutamos la funcion borrarDatos
+            borrarDatos();
+        }
+    }
+    
+    void borrarDatos() throws SQLException, InterruptedException, IOException {
+        // creamos una conexion y un statement
+        Connection connection = casino.crearConexion();
+        Statement statement = casino.crearStatement(connection);
+
+        // ejecutamos el script de creacion de la base de datos
+        try {
+            // Creamos un array con las sentencias SQL para crear la base de datos 'casino'
+            String[] sentencias = {
+                "DROP TABLE puntos;",
+                "DROP TABLE usuarios;",
+                "CREATE TABLE usuarios (id INT PRIMARY KEY, usuario VARCHAR(100), passwd VARCHAR(100));",
+                "CREATE TABLE puntos (id INT PRIMARY KEY, puntos INT, FOREIGN KEY (id) REFERENCES usuarios(id));",
+                "INSERT INTO usuarios (id, usuario, passwd) VALUES (1, 'paualdea', 'aldea2');",
+                "INSERT INTO puntos (id, puntos) VALUES (1, 12000);"
+            };
+
+            // Bucle for para iterar sobre todas las sentencias y ejecutarlas
+            for (int i = 0; i < sentencias.length; i++) {
+                statement.execute(sentencias[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR EN LA EJECUCION DE LA BASE DE DATOS");
+        }
+        
+        Thread.sleep(1750);
+        
+        statement.close();
+        connection.close();
+        
+        // Volvemos al menu inicio
+        boolean fullscreen = casino.getFullscreen();
+        
+        Stage stage;
+        Parent root;
+        
+        stage = (Stage) atras.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("PaginaInicio.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        
+        // Si fullscreen esta en true redimensionar todo
+        stage.setFullScreen(fullscreen);
+        casino.proporcionFullscreen(stage, root, fullscreen);
+        
+        stage.show();
     }
 }
